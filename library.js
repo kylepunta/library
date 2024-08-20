333710c928f1190b47fe86e9d7ffdc5a89315578
kylepunta <kylepurcell1998@gmail.com>
2024-08-20 16:04:17 +0100

    Changes made

--- library.js
+++ library.js
@@ -1,9 +1,10 @@
 const library = [];
 
-function Book (title, author, pages){
+function Book (title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 };
 
 function addBookToLibrary (book){
@@ -17,28 +18,36 @@ function addNewBook (newBook) {
     let bookCardTitle = document.createElement('div');
     let bookCardAuthor = document.createElement('div');
     let bookCardPages = document.createElement('div');
+    let bookCardRead = document.createElement('div');
 
     bookCard.classList.add('book-card');
     bookCardTitle.classList.add('book-card-title');
     bookCardAuthor.classList.add('book-card-author');
     bookCardPages.classList.add('book-card-pages');
+    bookCardRead.classList.add('book-card-read');
     
     bookList.appendChild(bookCard);
     bookCard.appendChild(bookCardTitle);
     bookCard.appendChild(bookCardAuthor);
     bookCard.appendChild(bookCardPages);
+    bookCard.appendChild(bookCardRead);
 
     let titleHeading = document.createElement('h3');
     let authorHeading = document.createElement('h3');
     let pagesHeading = document.createElement('h3');
+    let readHeading = document.createElement('h3');
 
     let bookTitle = document.createElement('p');
     let bookAuthor = document.createElement('p');
     let bookPages = document.createElement('p');
+    let readBtn = document.createElement('button');
+
+    readBtn.classList.add('read-button');
 
     titleHeading.textContent = "Title: ";
     authorHeading.textContent = "Author: ";
     pagesHeading.textContent = "Pages: ";
+    readHeading.textContent = "Read: ";
 
     bookTitle.textContent = newBook.title;
     bookAuthor.textContent = newBook.author;
@@ -50,6 +59,8 @@ function addNewBook (newBook) {
     bookCardAuthor.appendChild(bookAuthor);
     bookCardPages.appendChild(pagesHeading);
     bookCardPages.appendChild(bookPages);
+    bookCardRead.appendChild(readHeading);
+    bookCardRead.appendChild(readBtn);
 
     let bookCardDelete = document.createElement('div');
     bookCardDelete.classList.add('book-card-delete');
@@ -72,9 +83,20 @@ function addNewBook (newBook) {
     }
 
     bookCard.setAttribute('data-index', bookIndex);
+    bookCard.setAttribute('data-title', newBook.title);
 
     deleteBookBtn.addEventListener('click', () => {
-        deleteBook(bookIndex);
+        deleteBook(bookCard.getAttribute('data-index'));
+    })
+
+    readBtn.addEventListener('click', () => {
+        newBook.toggleReadStatus();
+        if (newBook.read === true){
+            readBtn.classList.add('is-read');
+        }
+        else {
+            readBtn.classList.remove('is-read');
+        }
     })
 };
 
@@ -88,12 +110,16 @@ function deleteBook (bookIndex) {
     library.splice(bookIndex, 1);
     let bookToDelete = document.querySelector(`[data-index="${bookIndex}"]`);
     bookToDelete.remove();
+    let books = document.querySelectorAll('.book-card');
+    books.forEach((book, index) => {
+        book.setAttribute('data-index', index);
+    });
     totalBooks.textContent = library.length;
 };
 
-const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', 310);
-const bookTwo = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180);
-const bookThree = new Book('To Kill A Mockingbird', 'Harper Lee', 281);
+const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', 310, false);
+const bookTwo = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, false);
+const bookThree = new Book('To Kill A Mockingbird', 'Harper Lee', 281, false);
 
 addBookToLibrary(bookOne);
 addBookToLibrary(bookTwo);
@@ -105,6 +131,7 @@ const openDialog = document.querySelector('.open-dialog');
 const formDialog = document.querySelector('.form-dialog');
 const closeDialog = document.querySelector('.close-dialog');
 const submitBtn = document.querySelector('.submit');
+const readBtns = document.querySelectorAll('.read-button');
 const form = document.querySelector('form');
 const totalBooks = document.querySelector('.book-numbers');
 totalBooks.textContent = library.length;
@@ -130,3 +157,7 @@ submitBtn.addEventListener('click', () => {
     addNewBook(newBook);
     totalBooks.textContent = library.length;
 });
+
+Book.prototype.toggleReadStatus = function (){
+    this.read = !this.read;
+};
